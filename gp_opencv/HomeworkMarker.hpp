f3189708521709eb88a8f633954bbdcfc88a9be1
bacon8282 <654321oiuytr@gmail.com>
2020-11-11 13:17:39 +0900

    submit

--- gp_opencv/HomeworkMarker.hpp
+++ gp_opencv/HomeworkMarker.hpp
@@ -30,6 +30,31 @@ protected:
 		// 결과값은 각각 out_rvec, out_tvec 매개변수에 대입하세요
 		//
 		/////////////////////////////////////////////////////////////////////////////////////////
+
+		cv::Mat markerImage;
+		cv::aruco::drawMarker(dictionary, 100, 256, markerImage, 1);
+		cv::imshow("id: 100, size: 256x256", markerImage);
+
+		cv::imwrite("res\\marker.jpg", markerImage);
+
+		std::vector<std::vector<cv::Point2f>> corners, rejected;
+		std::vector<int> ids;
+		cv::aruco::detectMarkers(in_frame, dictionary, corners, ids, detectorParams, rejected, cameraMatrix, distCoeffs);
+
+		std::vector<cv::Vec3d> rvecs, tvecs;
+		if (ids.size() > 0) {
+			cv::aruco::estimatePoseSingleMarkers(
+				corners, 0.07, cameraMatrix, distCoeffs, rvecs, tvecs);
+		}
+
+		//if (rvecs.size() > 0)
+			out_rvec = rvecs[0];
+		//if (tvecs.size() > 0)
+			out_tvec = tvecs[0];
+
+		//out_rvec = rvecs[0];
+		//out_tvec = tvecs[0];
+		//cv::aruco::drawAxis(in_frame, cameraMatrix, distCoeffs, rvecs, tvecs, 25);
 	}
 
 private:
